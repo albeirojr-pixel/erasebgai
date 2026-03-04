680405fed91921e207b85636ea375a98b99abef5
AlbeiroLopez <albeiro.jr@gmail.com>
2026-03-04 09:02:34 -0500

    Fix API route - use CommonJS syntax

--- api/remove-bg.js
+++ api/remove-bg.js
@@ -1,10 +1,4 @@
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -71,4 +65,10 @@ export default async function handler(req, res) {
     console.error("Handler error:", err);
     return res.status(500).json({ error: err.message });
   }
-}
+};
+
+module.exports.config = {
+  api: {
+    bodyParser: false,
+  },
+};
